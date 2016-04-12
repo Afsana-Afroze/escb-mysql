87e3f2f1584fb41f9f485120727b2033694bc950
Afsana-Afroze <roze0432@gmail.com>
2016-04-12 19:32:36 +0600

    add

---
+++ mysql3/searchlist.php
@@ -0,0 +1,113 @@
+<?php 
+include_once "conn.php"; 
+$sql = "SELECT * FROM `MYfamily`";
+if(isset ($_GET['Search']))
+{
+
+$sql .=" WHERE firstname like  '"."%" .$_GET['Search']."%"."' or lastname like  '"."%" .$_GET['Search']."%"."'";
+}
+$result = $conn->query($sql);
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>myfamily</title>
+    
+    <!-- Bootstrap -->
+    <link href="../css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+  </head>
+  <body>
+    <nav class="navbar navbar-default">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="index.html">Library</a>
+        </div>
+        <div id="navbar" class="collapse navbar-collapse">
+          <ul class="nav navbar-nav pull-right">
+           
+            <li><a href="">Searchlist</a></li>
+            <li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">  New <span class="caret"></span></a>
+              <ul class="dropdown-menu">
+                <li>search<li>
+              </ul>
+            </li>
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </nav>
+  <div class="container">
+    <h1>Family Member</h1>
+    <p class="lead">We have <?php echo ($result->num_rows ? $result->num_rows : 0); ?> family member in our list.</p>
+    <form role="form" method="GET" style="width: 500px;"action="searchlist.php">
+        <div class="form-group">
+          <label for="name">Search:</label>
+          <input type="text" class="form-control" id="search" name="Search" required="" />
+        </div>
+        
+
+        <button type="submit" class="btn btn-default">Submit</button>
+      </form>
+    <?php if($result->num_rows > 0): ?>
+      
+      <table class="table table-bordered">
+            <thead>
+              <tr>
+                <th>id</th>
+                <th>userid</th>
+                <th>firstname</th>
+                <th>lastname</th>
+                <th>email</th>
+                <th>password</th>
+                <th>reg_date</th>
+              </tr>
+            </thead>
+            <tbody>
+              <?php 
+          while ( $row = $result->fetch_assoc() ) {
+            // echo $i . ". " . $row['name'] . "(" . $row['id'] . ")<br>";
+            echo "<tr>
+            <td>".$row['id']."</td>
+            <td>".$row['userid']."</td>
+            <td>".$row['firstname']."</td>
+            <td>".$row['lastname']."</td>
+            <td>".$row['email']."</td>
+            <td>".$row['password']."</td>
+            <td>".$row['reg_date']."</td>
+            </tr>";
+          }
+               ?>
+              
+            </tbody>
+          </table>
+
+    <?php endif; ?>
+
+
+  </div><!-- /.container -->
+
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="../js/bootstrap.min.js"></script>
+  </body>
+</html>
